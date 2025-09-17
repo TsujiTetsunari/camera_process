e7741347796132392d3e286a0a434a32f080bb07
TsujiTetsunari <tetsunari.tsuji@tadano.com>
2025-09-17 16:02:30 +0900

    add

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+class SetConfig():
+    dir_name = "./data"
+    extension_name = ".jpg" 
+    file_name_path_1 = os.path.join(dir_name, "1"+ extension_name)
+    file_name_path_2 = os.path.join(dir_name, "2"+ extension_name)
+    file_name_path_3 = os.path.join(dir_name, "2"+ extension_name)
+    file_name_path_4 = os.path.join(dir_name, "2"+ extension_name)
+    save_dir_name = "./save_data"
+    save_file_name_1 = os.path.join(save_dir_name, "1_grascale"+extension_name)
+
