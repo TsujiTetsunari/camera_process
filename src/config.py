093502788ec56ffb88361a828eb95709f0f477e5
TsujiTetsunari <tetsunari.tsuji@tadano.com>
2025-09-17 16:20:37 +0900

    add

--- src/config.py
+++ src/config.py
@@ -9,5 +9,6 @@ class SetConfig():
     file_name_path_3 = os.path.join(dir_name, "2"+ extension_name)
     file_name_path_4 = os.path.join(dir_name, "2"+ extension_name)
     save_dir_name = "./save_data"
-    save_file_name_1 = os.path.join(save_dir_name, "1_grascale"+extension_name)
-
+    save_file_name_1 = os.path.join(save_dir_name, "1_"+extension_name)
+    save_file_name_gr_1 = os.path.join(save_dir_name, "1_grascale"+extension_name)
+    save_file_name_no_1 = os.path.join(save_dir_name, "1_no"+extension_name)
